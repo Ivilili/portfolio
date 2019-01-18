9d42e23ccc138145d3c2500aef634706e407abb4
Ivana Livaja <ivana.livaja@hotmail.com>
2019-01-18 19:25:36 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+particlesJS('particles-js', {
+	particles: {
+		number: {
+			value: 100
+		},
+		color: {
+			value: '#fff'
+		},
+		opacity: {
+			value: 0.5,
+			random: true,
+			anim: {
+				enable: false,
+				speed: 1
+			}
+		},
+		size: {
+			value: 3,
+			random: false,
+			anim: {
+				enable: false,
+				speed: 30
+			}
+		}
+	}
+});
